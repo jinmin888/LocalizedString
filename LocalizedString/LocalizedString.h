b2a38a91cb4c92d88ba6d58a9b8bb8b1d860c1a8
MIN <min@min1.local>
2018-01-31 11:44:46 +0900

    Initial Commit

---
+++ LocalizedString/LocalizedString.h
@@ -0,0 +1,19 @@
+//
+//  LocalizedString.h
+//  LocalizedString
+//
+//  Created by MIN on 2018. 1. 31..
+//  Copyright © 2018년 MIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalizedString.
+FOUNDATION_EXPORT double LocalizedStringVersionNumber;
+
+//! Project version string for LocalizedString.
+FOUNDATION_EXPORT const unsigned char LocalizedStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizedString/PublicHeader.h>
+
+
